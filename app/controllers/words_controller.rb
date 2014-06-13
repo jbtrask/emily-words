5b665afe037cccd6e29739f0456ea57ead09b780
James Trask <jbt_5@yahoo.com>
2014-06-12 21:33:48 -0700

    fix typo in letters

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -4,7 +4,7 @@ class WordsController < ApplicationController
     %w{ c O m p a s I o N a t E },
     %w{ R _ P r E T t y S W _ _ },
     %w{ E M I L y _ _ _ w E a r },
-    %w{ a k o O v - - - e s F o },
+    %w{ a k o O v _ _ _ e s F o },
     %w{ t s H V c 2 _ 4 E o u s },
     %w{ l 6 K i n d _ m T M N e },
     %w{ v _ _ N 2 5 f R I e n D },
